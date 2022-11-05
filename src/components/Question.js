df28df20aa7ab04d2c7800019f1211bea46230a1
Martin <souzam@sonic.net>
2022-11-05 12:34:23 -0400

    commit tree 378904b5bd850b2dc3071bf41e8b0e635ea006e3

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import { cleanup } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setTimeRemaining(timeRemaining => timeRemaining - 1)
+    }, 1000)
+
+    return () => {clearTimeout(countdown)}
+  }, [])
+
+  if (timeRemaining === 0) {
+    setTimeRemaining(10)
+    onAnswered(false)
+  }
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
